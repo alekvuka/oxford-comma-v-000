0c35a92824b228174e8b05896900ec62a2f54cc0
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-28 23:29:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
 if array.size == 1
   return array[0]
 elsif array.size == 2
-  return "#{array[0] and array[1]}"
+  return "#{array[0]} and #{array[1]}"
 else
   last_element = array.pop
   phrase = array.join(", ")
