ab0f4b7b75a8e8619a00c481b27c75734e639b22
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-28 23:25:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
 
 
   new_array = array
-  new_array.pop
-  phrase = new_array.join(", ")
-  final_phrase ="#{phrase}, and #{array[-1]}"
+  array.pop
+  phrase = array.join(", ")
+  final_phrase ="#{phrase}, and #{new_array[-1]}"
   return final_phrase
 
 end
